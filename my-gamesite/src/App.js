3509a379f0b71a98388e1ba02b98a74f4f89fcf2
naveenkumarh1998@gmail.com <naveenkumarh1998@gmail.com>
2023-03-28 13:57:36 +0530

    app is modifed

--- my-gamesite/src/App.js
+++ my-gamesite/src/App.js
@@ -1,24 +1,16 @@
-//import {BrowserRouter,Route,Routes} from 'react';
-
- 
-import Main from './components/main/main';
-//import Cart from  './components/cart/cart'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cart from "./components/cart/cart";
+import Main from "./components/main/main";
 
 function App() {
   return (
-    // <BrowserRouter>
-    //     <Routes>
-    //         <Route path='/' element={<Main/>}>
-    //         </Route>
-    //         <Route path='/cart' element={<Cart/>}>
-
-    //         </Route>
-    //     </Routes>
-    // </BrowserRouter>
-    <>
-    <Main/>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main/>}></Route>
+        <Route path="/cart" element={<Cart/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
